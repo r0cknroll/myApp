07fad97ab9e584e71e5eb179bd13a6dba45d284b
unknown <jbulawan@.bookwhirl.local>
2014-06-17 11:12:04 +0800

    Initialize

---
+++ js/services.js
@@ -0,0 +1,26 @@
+angular.module('starter.services', [])
+
+/**
+ * A simple example service that returns some data.
+ */
+.factory('Friends', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var friends = [
+    { id: 0, name: 'Lorem 1' },
+    { id: 1, name: 'Lorem 2' },
+    { id: 2, name: 'Lorem 3' },
+    { id: 3, name: 'Lorem 5' }
+  ];
+
+  return {
+    all: function() {
+      return friends;
+    },
+    get: function(friendId) {
+      // Simple index lookup
+      return friends[friendId];
+    }
+  }
+});
